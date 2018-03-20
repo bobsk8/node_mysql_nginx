a9cf869ad028ff415fda5aa1ea2e0002a702467c
Rodrigo Viana Prado <rodrigo.viana@mjv.com.br>
2018-03-20 14:26:32 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+
+// app.get('/', function(req, res) {
+//     var connection = mysql.createConnection({
+//       host     : 'db_mysql',
+//       user     : 'mysql_user',
+//       password : 'mysql_pass'
+//     });
+//     connection.connect(function(err) {
+//         if (err) {
+//             res.send('Could not connect to MySQL ' + err.stack);
+//         } else {
+//             res.send('Connected to MySQL - Thread ' + connection.threadId);
+//         }
+//     });
+// });
+
+app.get('/', function(req, res) {
+    res.send('Foi');
+    console.log('Foiiii')
+});
+
+app.listen(3000);
